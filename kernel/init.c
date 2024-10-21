3b8e899f27a410dfc0f05bb03b0457f8e7b98ff8
HPX <3169289310@qq.com>
2024-10-21 19:56:12 +0800

    实现malloc

--- kernel/init.c
+++ kernel/init.c
@@ -1,8 +1,4 @@
 #include "init.h" 
-#include "print.h" 
-#include "interrupt.h" 
-#include "timer.h"
-#include "memory.h"
 
 
 /*负责初始化所有模块 */ 
--- kernel/init.h
+++ kernel/init.h
@@ -1,6 +1,11 @@
 #ifndef __KERNEL_INIT_H
 #define __KERNEL_INIT_H
 
+#include "print.h" 
+#include "interrupt.h" 
+#include "timer.h"
+#include "memory.h"
+
 void init_all(void);
 
 #endif
